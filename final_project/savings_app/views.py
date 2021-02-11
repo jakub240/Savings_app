4bb9dee7c64c47ce3e6e4d03edc8b2435333a3f3
Jakub Werder <jakub.werder@gmail.com>
2021-02-11 22:25:03 +0100

    setup

---
+++ final_project/savings_app/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+
+# Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request,'base.html')
+
+
+class PeriodsListView(View):
+    def get(self, request):
+        return render(request, 'periods.html')
